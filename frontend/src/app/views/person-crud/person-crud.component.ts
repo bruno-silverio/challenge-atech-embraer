2a498f4f09d9760a9d226b97d56e60c656158e5d
Bruno Silverio <bcamilo94@gmail.com>
2024-05-20 00:01:24 -0300

    fix: css

--- frontend/src/app/views/person-crud/person-crud.component.ts
+++ frontend/src/app/views/person-crud/person-crud.component.ts
@@ -3,12 +3,12 @@ import { Router } from '@angular/router'
 @Component({
   selector: 'app-person-crud',
   templateUrl: './person-crud.component.html',
-  styleUrl: './person-crud.component.sass'
+  styleUrl: './person-crud.component.css'
 })
 export class PersonCrudComponent implements OnInit {
 
   constructor(private router: Router) {
-
+    
   }
 
   ngOnInit(): void {
