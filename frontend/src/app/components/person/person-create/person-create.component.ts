df6f72732478670dc7d685ccf6e353a692e2a362
Bruno Silverio <bcamilo94@gmail.com>
2024-05-19 21:44:11 -0300

    feat: add form create person

--- frontend/src/app/components/person/person-create/person-create.component.ts
+++ frontend/src/app/components/person/person-create/person-create.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { PersonService } from '../person.service';
+import { Person } from '../person.model';
 
 @Component({
   selector: 'app-person-create',
   templateUrl: './person-create.component.html',
   styleUrl: './person-create.component.sass'
 })
-export class PersonCreateComponent {
+export class PersonCreateComponent implements OnInit {
+  person: Person = {
+    name: '',
+    email: '',
+    phone: null
+  }
 
+  constructor(private personService: PersonService,
+      private router: Router) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  createPerson(): void {
+    this.personService.create(this.person).subscribe(() => {
+      this.personService.showMessage('Cadastro criado!')
+      this.router.navigate(['/people'])
+    })
+
+  }
+
+  cancel(): void {
+    this.router.navigate(['/people'])
+  }
 }
